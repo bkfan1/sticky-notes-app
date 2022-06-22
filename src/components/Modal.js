3956fbcdac84e9a9c08dc555712a0d07537c1dd4
bkfan1 <blossomsc15@gmail.com>
2022-06-22 15:51:51 -0400

    Removed unused imports, formated code to improve readability

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,25 +1,18 @@
-import { useEffect } from "react";
+export default function Modal({
+  children,
+  showModal,
+  setShowModal,
+  handleClickModalBtn,
+}) {
+  if (!showModal) {
+    return null;
+  }
 
-
-export default function Modal({children, showModal, setShowModal, handleClickModalBtn}){
-
-    if(!showModal){return null;}
-
-
-    return(
-        <>
-        <div className="modalOverlay flex flex-col items-center justify-center fixed top-0 bottom-0 left-0 right-0 z-10 w-full h-screen">
-            
-            <div className="">
-            {children}
-
-            </div>
-
-            
-
-
-
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="modalOverlay flex flex-col items-center justify-center fixed top-0 bottom-0 left-0 right-0 z-10 w-full h-screen">
+        <div className="">{children}</div>
+      </div>
+    </>
+  );
 }
