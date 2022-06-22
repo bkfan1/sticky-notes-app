e7e03e9d9b89a9b874155c5cd6d796aeec9a617d
bkfan1 <blossomsc15@gmail.com>
2022-06-22 15:51:29 -0400

    Removed unused imports, formated code to improve readability

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,14 @@ import { useContext } from "react";
 import { NotesContext } from "../context/NotesContext";
 
 export default function Layout({ children }) {
-    const {searchValue,searchValueOnChange} = useContext(NotesContext);
+  const { searchValue, searchValueOnChange } = useContext(NotesContext);
   return (
     <>
       <nav className="flex flex-col items-center justify-center py-4 text-white bg-slate-800">
         <h1 className="text-2xl font-bold">Sticky Notes App</h1>
         <form className="my-1">
           <input
-          onChange={(e)=>searchValueOnChange(e)}
+            onChange={(e) => searchValueOnChange(e)}
             type="text"
             value={searchValue}
             placeholder="Search by term..."
