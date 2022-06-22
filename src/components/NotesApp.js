2384eb86e919794572d0f33b268a832d202dbe5f
bkfan1 <blossomsc15@gmail.com>
2022-06-22 15:53:02 -0400

    Removed unused imports, formated code to improve readability

--- src/components/NotesApp.js
+++ src/components/NotesApp.js
@@ -1,17 +1,12 @@
-import { useContext } from "react";
-import { NotesContext } from "../context/NotesContext";
 import Layout from "./Layout";
 import NotesGridContainer from "./NotesGridContainer";
-import PaginationMenu from "./PaginationMenu";
 
-
-export default function NotesApp(){
-    
-    return(
-        <>
-        <Layout>
-            <NotesGridContainer/>
-        </Layout>
-        </>
-    )
+export default function NotesApp() {
+  return (
+    <>
+      <Layout>
+        <NotesGridContainer />
+      </Layout>
+    </>
+  );
 }
