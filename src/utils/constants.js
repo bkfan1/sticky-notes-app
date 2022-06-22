292a1e123ace0e24b026dd409634615dc824d41a
bkfan1 <blossomsc15@gmail.com>
2022-06-22 15:50:49 -0400

    Created folder, added him constants.js file

---
+++ src/utils/constants.js
@@ -0,0 +1,73 @@
+export const initialNotes = [
+    {
+      id: "we41984z",
+      title: "Web technologies",
+      body: `
+    - HTML
+    - CSS
+    - Javascript
+    - React`,
+      createdAt: "21/6/2022",
+    },
+  
+    {
+      id: "asd459zcj",
+      title: "Todo",
+      body: `
+    - [x] Learn data structures
+    - [x] Learn algorithms
+    - [ ] Learn Big O notation
+    `,
+      createdAt: "21/6/2022",
+    },
+    {
+      id: "cjknfdf348",
+      title: "Best quotes",
+      body: `
+    > This is a quote - Somebody
+  
+    > Hello world - Your first computer program
+  
+    > Hire me - Jackson Paredes Ferranti (@bkfan1)
+    `,
+      createdAt: "21/6/2022",
+    },
+    {
+      id: "jjfhei13",
+      title: "Headings",
+      body: `
+    # Heading 1
+    ## Heading 2
+    ### Heading 3
+    ### Heading 4
+    #### Heading 5
+    ##### Heading 6
+    
+    `,
+      createdAt: "21/6/2022",
+    },
+  
+    {
+      id: "ksalwei35688",
+      title: "Bold and Italic",
+      body: `
+    The quick brown fox *jumps over* the lazy dog
+    The quick brown fox **jumps over** the lazy dog
+    
+    `,
+      createdAt: "21/6/2022",
+    },
+    {
+      id: "wajieox48456889",
+      title: "Useful links",
+      body: `
+    [Google](https://www.google.com)
+    [Youtube](https://www.youtube.com)
+    [Bkfan1 Profile](https://github.com/bkfan1)
+    
+    `,
+      createdAt: "21/6/2022",
+    },
+  
+    {id:"linuxTorvalds99", title:"A picture", body:"![Tux, the Linux mascot](https://upload.wikimedia.org/wikipedia/commons/a/af/Tux.png?20070323032439)", createdAt:"21/6/2022"}
+  ];
