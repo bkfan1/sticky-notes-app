7ad34379575e1d86466d0222f0105cb7e30905db
bkfan1 <blossomsc15@gmail.com>
2022-07-04 14:08:08 -0400

    Changed template strings to simple strings

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,70 +2,39 @@ export const initialNotes = [
     {
       id: "we41984z",
       title: "Web technologies",
-      body: `
-    - HTML
-    - CSS
-    - Javascript
-    - React`,
+      body: '- HTML\n- CSS\n- Javascript\n- React',
       createdAt: "21/6/2022",
     },
   
     {
       id: "asd459zcj",
       title: "Todo",
-      body: `
-    - [x] Learn data structures
-    - [x] Learn algorithms
-    - [ ] Learn Big O notation
-    `,
+      body: '- [x] Learn data structures\n- [x] Learn algorithms\n- [ ] Learn Big O notation',
       createdAt: "21/6/2022",
     },
     {
       id: "cjknfdf348",
       title: "Best quotes",
-      body: `
-    > This is a quote - Somebody
-  
-    > Hello world - Your first computer program
-  
-    > Hire me - Jackson Paredes Ferranti (@bkfan1)
-    `,
+      body: '> This is a quote - Somebody\n\n> Hello world - Your first computer program\n\n> Hire me - Jackson Paredes Ferranti (@bkfan1)',
       createdAt: "21/6/2022",
     },
     {
       id: "jjfhei13",
       title: "Headings",
-      body: `
-    # Heading 1
-    ## Heading 2
-    ### Heading 3
-    ### Heading 4
-    #### Heading 5
-    ##### Heading 6
-    
-    `,
+      body: '# Heading 1\n## Heading 2\n### Heading 3\n### Heading 4\n#### Heading 5\n##### Heading 6',
       createdAt: "21/6/2022",
     },
   
     {
       id: "ksalwei35688",
       title: "Bold and Italic",
-      body: `
-    The quick brown fox *jumps over* the lazy dog
-    The quick brown fox **jumps over** the lazy dog
-    
-    `,
+      body: 'The quick brown fox *jumps over* the lazy dog\nThe quick brown fox **jumps over** the lazy dog',
       createdAt: "21/6/2022",
     },
     {
       id: "wajieox48456889",
       title: "Useful links",
-      body: `
-    [Google](https://www.google.com)
-    [Youtube](https://www.youtube.com)
-    [Bkfan1 Profile](https://github.com/bkfan1)
-    
-    `,
+      body: '- [Google](https://www.google.com)\n- [Youtube](https://www.youtube.com)\n- [Bkfan1 Profile](https://github.com/bkfan1)',
       createdAt: "21/6/2022",
     },
   
